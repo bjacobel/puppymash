54c482e9a164ef65c8ebaf9391934254d5017950
Brian Jacobel <bjacobel@gmail.com>
2013-11-23 15:27:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puppymash::Application.config.secret_key_base = '15f6adfd02dc79e6f63132110fa16676020d0755999f029a49fd7542cf5f4842eccc6d2eefbe32613d7d9f567cb572a3ec4c6d14e94dd22f49864e98b3c84e66'
